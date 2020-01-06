9e1e3bff0403b9218b34d87427b6872288bc9b8a
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-01-06 21:54:31 +0000

    Add files via upload

--- activity3/linear_search_A.py
+++ activity3/linear_search_A.py
@@ -12,11 +12,11 @@ def linear_search(v, L):
 
 # Driver code ...
 print("%s\t\t %s\t\t %s" %("List Size", "Found Index", "#Comparisons"))
-for list_size in [1, 10, 1000, 10000, 100000, 1000000]:
+for list_size in [1, 10, 100, 1000, 10000, 100000, 1000000]:
     some_list = list(range(list_size))
     random.shuffle(some_list) # randomise the list
     comparisons = 0
-    # target = random.randrange(len(some_list)) # average case
+    #target = random.randrange(len(some_list)) # average case
     target = -1 # worst case because -1 never exists
     pos = linear_search(target, some_list)
     print("%d\t\t %d\t\t %d" %(len(some_list), pos, comparisons))
